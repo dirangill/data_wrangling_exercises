7f2495d5713d0e8596d17b90183786e75cd613c1
dirangill <diran.gill@keepmoat.com>
2022-03-21 16:36:07 +0000

    added loop to count user types

--- hitting_the_road_with_citibike.py
+++ hitting_the_road_with_citibike.py
@@ -1,7 +1,22 @@
 import csv
 
-source_file = open('chapter_2_data_files\\202009CitibikeTripdataExample.csv', 'r')
+source_file = open('chapter_2_data_files\\202009-citibike-tripdata.csv', 'r')
 
 citibike_reader = csv.DictReader(source_file)
 
 print(citibike_reader.fieldnames)
+
+subscriber_count = 0
+customer_count = 0
+other_user_count = 0
+
+for a_row in citibike_reader:
+    if a_row['usertype'] == 'Subscriber':
+        subscriber_count = subscriber_count + 1
+    elif a_row['usertype'] == 'Customer':
+        customer_count = customer_count + 1
+    else:
+        other_user_count = other_user_count + 1
+
+
+    
